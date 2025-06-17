26a4695888594246a544bacf9e800d5f5839153c
priyanka1970 <apkanherkar@gmail.com>
2025-06-17 23:36:16 +0530

    updated runner

--- src/test/java/Utils/TestRunner.java
+++ src/test/java/Utils/TestRunner.java
@@ -4,10 +4,6 @@ import io.cucumber.testng.AbstractTestNGCucumberTests;
 import io.cucumber.testng.CucumberOptions;
 import org.testng.annotations.BeforeSuite;
 
-import java.io.IOException;
-import java.net.HttpURLConnection;
-import java.net.URL;
-
 @CucumberOptions(
         features = "src/test/java/features", // Now picks all .feature files
         glue = "features.steps",
@@ -20,19 +16,4 @@ import java.net.URL;
         monochrome = true
 )
 public class TestRunner extends AbstractTestNGCucumberTests {
-
-        @BeforeSuite
-        public void checkFastApiHealth() throws IOException {
-                String healthCheckUrl = "http://127.0.0.1:8000/health"; // Replace if using ConfigUtil
-                HttpURLConnection connection = (HttpURLConnection) new URL(healthCheckUrl).openConnection();
-                connection.setRequestMethod("GET");
-                connection.connect();
-
-                int code = connection.getResponseCode();
-                if (code != 200) {
-                        throw new RuntimeException("❌ FastAPI server is not running (status code: " + code + ")");
-                } else {
-                        System.out.println("✅ FastAPI is up and healthy (status code: 200).");
-                }
-        }
 }
